4cd5bae0f14897e17e7d8921d1676349818a9c30
Girish <m.girishroy@gmail.com>
2023-09-22 22:39:57 +0530

    inital setup

---
+++ app/restaurant/[name]/page.tsx
@@ -0,0 +1,37 @@
+import NavBar from "@/app/components/NavBar";
+import Link from "next/link";
+import React from "react";
+import Header from "./components/Header";
+import RestaurantNavBar from "./components/RestaurantNavBar";
+import Title from "./components/Title";
+import Ratings from "./components/Ratings";
+import Description from "./components/Description";
+import Images from "./components/Images";
+import Reviews from "./components/Reviews";
+import ReservationCard from "./components/ReservationCard";
+
+function RestaurantDetailsPage() {
+  return (
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        <NavBar />
+        <Header />
+        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+          <div className="bg-white w-[70%] rounded p-3 shadow">
+            <RestaurantNavBar />
+            <Title />
+            <Ratings />
+            <Description />
+            <Images />
+            <Reviews />
+          </div>
+          <div className="w-[27%] relative text-reg">
+            <ReservationCard />
+          </div>
+        </div>
+      </main>
+    </main>
+  );
+}
+
+export default RestaurantDetailsPage;
