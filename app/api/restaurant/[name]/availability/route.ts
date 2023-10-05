ecc14961466f4f92faf883c7391bb0f3dd3d451d
Girish <m.girishroy@gmail.com>
2023-10-05 18:09:40 +0530

    adding booking data and logic

--- app/api/restaurant/[name]/availability/route.ts
+++ app/api/restaurant/[name]/availability/route.ts
@@ -30,5 +30,64 @@ export async function GET(request: NextRequest, { params }: any) {
         }, { status: 400 })
     }
 
-    return NextResponse.json({ searchTimes })
+    const bookings = await prisma.booking.findMany({
+        where: {
+            booking_time: {
+                gte: new Date(`${day}T${searchTimes[0]}`),
+                lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`)
+            }
+        },
+        select: {
+            number_of_people: true,
+            booking_time: true,
+            tables: true,
+        }
+    })
+
+    const bookingTableObj: { [key: string]: { [key: number]: true } } = {}
+
+    bookings.forEach(booking => {
+        bookingTableObj[booking.booking_time.toISOString()] = booking.tables.reduce((obj, table) => {
+            return {
+                ...obj,
+                [table.table_id]: true
+            }
+        }, {})
+    })
+
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug: name
+        },
+        select: {
+            tables: true
+        }
+    })
+
+    if (!restaurant) {
+        return NextResponse.json({
+            errorMessage:
+                "Invalid data provided"
+        }, { status: 400 })
+    }
+
+    const tables = restaurant.tables;
+
+    const searchTimesWIthTables = searchTimes.map(searchTime => {
+        return {
+            date: new Date(`${day}T${searchTimes}`),
+            time: searchTime,
+            tables
+        }
+    });
+
+    searchTimesWIthTables.forEach((t) => t.tables = t.tables.filter(table => {
+        if (bookingTableObj[t.date.toISOString()]) {
+            if (bookingTableObj[t.date.toISOString()][table.id]) {
+                return false
+            }
+            return true;
+        }
+    }))
+    return NextResponse.json({ searchTimes, bookingTableObj, tables, searchTimesWIthTables })
 }
